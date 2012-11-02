d2037b989d83f9176d2156a4c00404e1dfede5fa
Stuart Rackham <srackham@gmail.com>
2012-11-03 08:53:17 +1300

    Docs and tweaks.

--- toaster.js
+++ toaster.js
@@ -1,10 +1,20 @@
+//
+// Toast popup notifier
+//
+// By: Stuart Rackham
+//
+// Inspired by: https://github.com/Srirangan/notifer.js
+//              https://github.com/CodeSeven/toastr
+//
+
 var Toaster = (function($){
 
   /* Private variables and functions */
 
+  // Exported as Toaster.defaults
   var _defaults = {
     displayDuration: 2000,    // In milliseconds, set to 0 to make sticky.
-    fadeoutDuration: 800,     // In milliseconds.
+    fadeOutDuration: 800,     // In milliseconds.
     toastClass: '',   // CSS class name.
     toastCSS: {},     // Override .toast properties in toast.css
     containerCSS: {}  // Override #toast-container properties in toast.css
@@ -35,15 +45,14 @@ var Toaster = (function($){
     }
     if (options.displayDuration > 0) {
       setTimeout(function() {
-        toastElement.fadeOut(options.fadeoutDuration, function() {
+        toastElement.fadeOut(options.fadeOutDuration, function() {
           toastElement.remove();
         });
       }, options.displayDuration);
     }
-    toastElement.bind('click', function() {
+    toastElement.on('click', function() {
       toastElement.remove();
     });
-    //toastElement.append(textElement);
     _container.prepend(toastElement);
   }
 
@@ -52,10 +61,21 @@ var Toaster = (function($){
 
   return {
 
-    // Default CSS and animation parameters.
+    // Toaster.defaults
+    // Modifiable default CSS and animation parameters.
     defaults: _defaults,
 
-    // Display functions.
+    // Popup functions:
+    //
+    // Toaster.info    (message [,title [, options]])
+    // Toaster.warning (message [,title [, options]])
+    // Toaster.error   (message [,title [, options]])
+    // Toaster.success (message [,title [, options]])
+    //
+    // message: String
+    // title:   String
+    // options: Object with properties to override Toaster.defaults
+
     info: function(message, title, options) {
       _notify(message, title,
           $.extend({}, {toastClass: 'toast-info'}, options));
