7b9d377a7385e1576aa4b8b721960e60da5af237
Stuart Rackham <srackham@gmail.com>
2014-10-27 09:14:16 +1300

    Test custom toast width.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,28 @@
+var TOAST_SELECTOR = 'body div#toast-container div.toast-info';
+
 QUnit.test('Sticky toast', function(assert) {
   Toast.info('Message', 'Sticky toast', {displayDuration: 0});
-  var container = $('body div#toast-container div.toast-info');
+  var container = $(TOAST_SELECTOR);
   assert.ok(container.length > 0, 'Create toast container element');
   assert.ok(container.find('div.toast-title').text() === 'Sticky toast', 'Toast title');
   assert.ok(container.find('div.toast-message').text() === 'Message', 'Toast message');
   container.trigger('click');
-  container = $('body div#toast-container div.toast-info');
+  container = $(TOAST_SELECTOR);
   assert.ok(container.length === 0, 'Delete toast container element');
 });
 
 QUnit.test('Timeout toast', function(assert) {
+  Toast.defaults.width = '600px';
   Toast.info('Message', 'Timeout toast');
-  var container = $('body div#toast-container div.toast-info');
+  var container = $(TOAST_SELECTOR);
   assert.ok(container.length > 0, 'Create toast container element');
   assert.ok(container.find('div.toast-title').text() === 'Timeout toast', 'Toast title');
   assert.ok(container.find('div.toast-message').text() === 'Message', 'Toast message');
-  // Stop test and wait for toast to fade away.
+  assert.ok(container.parent().css('width') === Toast.defaults.width, 'Toast width');
+  // Stop test and wait for toast to timeout and close.
   stop();
   setTimeout(function() {
-    container = $('body div#toast-container div.toast-info');
+    container = $(TOAST_SELECTOR);
     assert.ok(container.length === 0, 'Delete toast container element');
     start();  // Restart test.
   }, Toast.defaults.displayDuration + Toast.defaults.fadeOutDuration + 100);
