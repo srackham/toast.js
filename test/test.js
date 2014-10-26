7c012732fec3a9158caaac6db382a7f2ee1ddb3d
Stuart Rackham <srackham@gmail.com>
2014-10-27 09:52:28 +1300

    Change ok() assertions to equal() as they provide more dianostic information on failure.

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ var TOAST_SELECTOR = 'body div#toast-container div.toast-info';
 QUnit.test('Sticky toast', function(assert) {
   Toast.info('Message', 'Sticky toast', {displayDuration: 0});
   var container = $(TOAST_SELECTOR);
-  assert.ok(container.length > 0, 'Create toast container element');
-  assert.ok(container.find('div.toast-title').text() === 'Sticky toast', 'Toast title');
-  assert.ok(container.find('div.toast-message').text() === 'Message', 'Toast message');
+  assert.equal(container.length, 1, 'Create toast');
+  assert.equal(container.find('div.toast-title').text(), 'Sticky toast', 'Toast title');
+  assert.equal(container.find('div.toast-message').text(), 'Message', 'Toast message');
   container.trigger('click');
   container = $(TOAST_SELECTOR);
-  assert.ok(container.length === 0, 'Delete toast container element');
+  assert.equal(container.length, 0, 'Click deletes toast');
 });
 
 QUnit.test('Timeout toast', function(assert) {
@@ -17,18 +17,18 @@ QUnit.test('Timeout toast', function(assert) {
   Toast.defaults.fadeOutDuration = 300;
   Toast.info('Message', 'Timeout toast');
   var container = $(TOAST_SELECTOR);
-  assert.ok(container.length > 0, 'Create toast container element');
-  assert.ok(container.find('div.toast-title').text() === 'Timeout toast', 'Toast title');
-  assert.ok(container.find('div.toast-message').text() === 'Message', 'Toast message');
-  assert.ok(container.parent().css('width') === Toast.defaults.width, 'Toast width');
+  assert.equal(container.length, 1, 'Create toast');
+  assert.equal(container.find('div.toast-title').text(), 'Timeout toast', 'Toast title');
+  assert.equal(container.find('div.toast-message').text(), 'Message', 'Toast message');
+  assert.equal(container.parent().css('width'), Toast.defaults.width, 'Toast width');
   // Stop test and wait for toast to timeout and close.
   stop();
   setTimeout(function() {
     container = $(TOAST_SELECTOR);
-    assert.ok(container.length > 0, 'Toast not yet timed out');
+    assert.equal(container.length, 1, 'Toast not yet timed out');
     setTimeout(function() {
       container = $(TOAST_SELECTOR);
-      assert.ok(container.length === 0, 'Toast timed out');
+      assert.equal(container.length, 0, 'Toast timed out');
       start();  // Restart test.
     }, 200);
   }, Toast.defaults.displayDuration + Toast.defaults.fadeOutDuration - 100);
